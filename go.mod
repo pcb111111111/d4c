fed64cb064a9e9b8bd9ecce3dbced892957e7200
peichaobo <peichaobo@wsdashi.com>
2022-11-28 01:32:49 -0800

    11

---
+++ go.mod
@@ -0,0 +1,3 @@
+module d4c
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
+)
+
+// generate random data for line chart
+func generateLineItems() []opts.LineData {
+	items := make([]opts.LineData, 0)
+	for i := 0; i < 8; i++ {
+		items = append(items, opts.LineData{Value: rand.Intn(300)})
+	}
+	return items
+}
+
+func httpserver(w http.ResponseWriter, _ *http.Request) {
+	// create a new line instance
+	line := charts.NewLine()
+	// set some global options like Title/Legend/ToolTip or anything else
+	line.SetGlobalOptions(
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Line example in Westeros theme",
+			Subtitle: "Line chart rendered by the http server this time",
+		}))
+
+	// Put data into instance
+	line.SetXAxis([]string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", " 22"}).
+		AddSeries("Category A", generateLineItems()).
+		AddSeries("Category B", generateLineItems()).
+		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+
+	a := opts.YAxis{
+		Name: "ss",
+		Type: "str",
+		Show: true,
+		Data: 1,
+	}
+
+	c := charts.XYAxis{
+		XAxisList: nil,
+		YAxisList: nil,
+	}
+
+	c.ExtendYAxis(a)
+	
+	line.Render(w)
+	time.Sleep(1 * time.Second)
+
+}
+
+func main() {
+	http.HandleFunc("/", httpserver)
+	http.ListenAndServe(":8081", nil)
+}
